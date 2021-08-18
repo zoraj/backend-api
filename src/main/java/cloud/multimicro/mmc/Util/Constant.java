0043ffc3623f7cb73297fe20d62da746bfff24b7
DESKTOP-71HOIB2\herizo <herizo@multimicro.fr>
2021-08-18 10:58:27 +0300

    MOD: changement appelle PEPPER

--- src/main/java/cloud/multimicro/mmc/Util/Constant.java
+++ src/main/java/cloud/multimicro/mmc/Util/Constant.java
@@ -4,10 +4,6 @@ package cloud.multimicro.mmc.Util;
  * Constant
  */
 public class Constant {
-
-    public static final String MMC_PEPPER = "WMb6fm6EcMrpeCShLEscWx";
-
-    public static final String MMC_JWT_SECRET_KEY = "mnQWM84EdocoK7Vd72Nke7HGDrL8zWteUQdE9DULLGVhVZ3oxAb3fxrLVgeZt5jprz4KVvAhuQs2zCGP4R9gmv2MimAunr";
     public static final String MMC_JWT_ID = "cloud.multimicro";
     public static final String MMC_JWT_ISSUER = "MMC";
     public static final String MMC_JWT_SUBJECT = "MMC Token for backend";
