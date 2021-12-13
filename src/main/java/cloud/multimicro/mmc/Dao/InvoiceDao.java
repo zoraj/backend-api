090576e75af977377d21bf5c20981e2f6ce79a99
DESKTOP-71HOIB2\herizo <herizo@multimicro.fr>
2021-12-13 09:07:28 +0300

    Revert "ADD:ws des factures"
    This reverts commit e7e3bcaeccf63f6ee6e2fb4b5f9bcf7920118f8e.

--- src/main/java/cloud/multimicro/mmc/Dao/InvoiceDao.java
+++ src/main/java/cloud/multimicro/mmc/Dao/InvoiceDao.java
@@ -5,18 +5,15 @@
  */
 package cloud.multimicro.mmc.Dao;
 
-import cloud.multimicro.mmc.Entity.TPmsFacture;
-import cloud.multimicro.mmc.Entity.TPmsFactureDetail;
-import cloud.multimicro.mmc.Entity.TPosFacture;
-import cloud.multimicro.mmc.Entity.TPosFactureDetail;
+import cloud.multimicro.mmc.Entity.VPmsFacture;
 import cloud.multimicro.mmc.Entity.VPmsFactureDetail;
-import cloud.multimicro.mmc.Exception.CustomConstraintViolationException;
 import java.util.List;
+import java.util.Objects;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.MultivaluedMap;
 
 
 /**
@@ -28,81 +25,73 @@ public class InvoiceDao {
     @PersistenceContext
     EntityManager entityManager;
     
-    public List<TPmsFacture> getTpmsFacture() {
-        List<TPmsFacture> facture = entityManager.createQuery(" FROM TPmsFacture  ").getResultList();
-        return facture;
-    }
-    
-     public TPmsFacture getNumFacture(String numInvoice)throws NoResultException {
-        TPmsFacture result = (TPmsFacture) entityManager.createQuery("FROM TPmsFacture WHERE numero =:numInvoice")
+    public VPmsFacture getInvoiceHeader(String numInvoice)throws NoResultException {
+        VPmsFacture result = (VPmsFacture) entityManager.createQuery("FROM VPmsFacture WHERE numFacture =:numInvoice")
                 .setParameter("numInvoice", numInvoice)
                 .getSingleResult();
-        return result;
-    }
-     
-    public List<TPmsFactureDetail> getInvoiceDetail(String numInvoice)throws NoResultException {
-        List<TPmsFactureDetail> result = entityManager.createQuery("FROM TPmsFactureDetail WHERE pmsFactureNumero =:numInvoice")
-            .setParameter("numInvoice", numInvoice)
-            .getResultList();
         return result;
 
     }
     
-    public void setTpmsFacture(TPmsFacture facture) throws CustomConstraintViolationException {
-        try{
-        entityManager.persist(facture);
-        } catch (ConstraintViolationException ex) {
-            throw new CustomConstraintViolationException(ex);
-        }
-    }
-     
-    public List<TPmsFactureDetail> getTpmsFactureDetail() {
-        List<TPmsFactureDetail> customer = entityManager.createQuery(" FROM TPmsFactureDetail  ").getResultList();
-        return customer;
-    }
-    
-    public void setTpmsFactureDetail(TPmsFactureDetail facture) throws CustomConstraintViolationException {
-        try{
-        entityManager.persist(facture);
-        } catch (ConstraintViolationException ex) {
-            throw new CustomConstraintViolationException(ex);
-        }
-    }
-     
-    public List<TPosFacture> getTposFacture() {
-        List<TPosFacture> customer = entityManager.createQuery(" FROM TPosFacture  ").getResultList();
-        return customer;
-    }
-    
-    public void setTposFacture(TPosFacture facture) throws CustomConstraintViolationException {
-        try{
-        entityManager.persist(facture);
-        } catch (ConstraintViolationException ex) {
-            throw new CustomConstraintViolationException(ex);
-        }
-    }
-    
-    public List<TPosFactureDetail> getTposFactureDetail() {
-        List<TPosFactureDetail> customer = entityManager.createQuery(" FROM TPosFactureDetail  ").getResultList();
-        return customer;
-    }
-    
-    public void setTposFactureDetail(TPosFactureDetail factureDetail) throws CustomConstraintViolationException {
-        try{
-        entityManager.persist(factureDetail);
-        } catch (ConstraintViolationException ex) {
-            throw new CustomConstraintViolationException(ex);
-        }
-    }
-    
-    /*public List<VPmsFactureDetail> getInvoiceDetail(String numInvoice)throws NoResultException {
+    public List<VPmsFactureDetail> getInvoiceDetail(String numInvoice)throws NoResultException {
         List<VPmsFactureDetail> result = entityManager.createQuery("FROM VPmsFactureDetail WHERE numFacture =:numInvoice")
             .setParameter("numInvoice", numInvoice)
             .getResultList();
         return result;
 
-    }*/
+    }
     
+    public VPmsFacture getByEnteteId(int id){     
+        VPmsFacture invoice = entityManager.find(VPmsFacture.class, id);
+        return invoice;
+    }
     
-    
+    public List<VPmsFacture> getAll(MultivaluedMap<String, String> list) {
+
+        String numFacture = list.getFirst("numFacture");
+        String datenote = list.getFirst("datenote");
+        String customer = list.getFirst("customer");
+        String status = list.getFirst("statutPointage");
+        
+        Boolean isExist = false;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("FROM VPmsFacture  ");
+     
+
+        if (!Objects.isNull(numFacture)) {
+            stringBuilder.append(" WHERE numFacture = '" + numFacture + "'");
+            isExist = true;
+        }
+
+        if (!Objects.isNull(datenote)) {
+            if (isExist == true) {
+                stringBuilder.append(" AND dateNote <= '" + datenote + "'");
+            } else {
+                stringBuilder.append(" WHERE  dateNote <= '" + datenote + "'");
+                isExist = true;
+            }
+        }
+
+        if (!Objects.isNull(customer)) {
+            if (isExist == true) {
+                stringBuilder.append(" AND mmcClientId = " + customer );
+            } else {
+                stringBuilder.append(" WHERE mmcClientId = " + customer);
+                isExist = true;
+            }
+        }
+
+        if (!Objects.isNull(status)) {
+            if (isExist == true) {
+                stringBuilder.append(" AND statutPointage IS NULL OR statutPointage = 1 ");
+            } else {
+                stringBuilder.append(" WHERE statutPointage  IS NULL OR statutPointage = 1 ");
+                isExist = true;
+            }
+        }
+        
+        
+        List<VPmsFacture> result = entityManager.createQuery(stringBuilder.toString()).getResultList();
+        return result;
+    }
 }
--- src/main/java/cloud/multimicro/mmc/Service/InvoiceService.java
+++ src/main/java/cloud/multimicro/mmc/Service/InvoiceService.java
@@ -6,21 +6,15 @@
 package cloud.multimicro.mmc.Service;
 
 import cloud.multimicro.mmc.Dao.InvoiceDao;
-import cloud.multimicro.mmc.Entity.TPmsFacture;
-import cloud.multimicro.mmc.Entity.TPmsFactureDetail;
-import cloud.multimicro.mmc.Entity.TPosFacture;
-import cloud.multimicro.mmc.Entity.TPosFactureDetail;
 import cloud.multimicro.mmc.Entity.VPmsEditionDepartPrevu;
-//import cloud.multimicro.mmc.Entity.VPmsFacture;
+import cloud.multimicro.mmc.Entity.VPmsFacture;
 import cloud.multimicro.mmc.Entity.VPmsFactureDetail;
-import cloud.multimicro.mmc.Exception.CustomConstraintViolationException;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -43,9 +37,23 @@ public class InvoiceService {
     
     private static final Logger LOGGER = Logger.getLogger(ProductService.class);
     @Inject
-    InvoiceDao invoiceDao;   
+    InvoiceDao invoiceDao;
     
-    /*@GET
+    @GET
+    @Path("/pms/invoice-header")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getInvoiceHeader(@Context UriInfo info) {
+        try {
+            String numInvoice = info.getQueryParameters().getFirst("numFacture");
+            VPmsFacture invoice  = invoiceDao.getInvoiceHeader(numInvoice);
+            return Response.ok(invoice, MediaType.APPLICATION_JSON).build();
+        }
+        catch(NoResultException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.toString()).build();
+        }
+    }
+    
+    @GET
     @Path("/pms/invoice-detail")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getByNumInvoice(@Context UriInfo info) {
@@ -57,135 +65,28 @@ public class InvoiceService {
         catch(NoResultException e) {
             return Response.status(Response.Status.NOT_FOUND).entity(e.toString()).build();
         }
-    }*/
-    
-    @GET
-    @Path("/pms/invoice-detail")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getByNumInvoice(@Context UriInfo info) {
-        try {
-            String numInvoice = info.getQueryParameters().getFirst("numFacture");
-            List<TPmsFactureDetail> invoice  = invoiceDao.getInvoiceDetail(numInvoice);
-            return Response.ok(invoice, MediaType.APPLICATION_JSON).build();
-        }
-        catch(NoResultException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(e.toString()).build();
-        }
     }
     
     @GET
-    @Path("/pms/facture")
+    @Path("/pms/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getTpmsFacture() {
-         List<TPmsFacture> facture = invoiceDao.getTpmsFacture();        
-        if (facture == null) {
+    public Response getByEnteteId(@PathParam("id") int id) {
+        VPmsFacture invoice = invoiceDao.getByEnteteId(id);
+        if (invoice == null) {
             throw new NotFoundException();
             //return Response.status(Response.Status.NOT_FOUND).entity("Object not found.").build();  
         }
-        return Response.ok(facture, MediaType.APPLICATION_JSON).build();   
+        return Response.ok(invoice, MediaType.APPLICATION_JSON).build();
     }
     
     @GET
-    @Path("/pms/invoice-header")
+    @Path("/pms")  
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getNumFacture(@Context UriInfo info) {
-        try {
-            String numInvoice = info.getQueryParameters().getFirst("numFacture");
-            TPmsFacture invoice  = invoiceDao.getNumFacture(numInvoice);
-            return Response.ok(invoice, MediaType.APPLICATION_JSON).build();
-        }
-        catch(NoResultException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(e.toString()).build();
-        }
-    }
-    
-    
-    @Path("/pms/facture")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addTpmsFacture(TPmsFacture facture) {
-        try {
-            invoiceDao.setTpmsFacture(facture);
-            return Response.status(Response.Status.CREATED).entity(facture).build();
-        } catch (CustomConstraintViolationException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
-    }
-    
-    @GET
-    @Path("/pms/facture-detail")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTpmsFactureDetail() {
-         List<TPmsFactureDetail> facture = invoiceDao.getTpmsFactureDetail();        
-        if (facture == null) {
+    public Response getAll(@Context UriInfo info) {        
+        List<VPmsFacture> invoiceList = invoiceDao.getAll(info.getQueryParameters());
+        if (invoiceList.isEmpty()) {
             throw new NotFoundException();
-            //return Response.status(Response.Status.NOT_FOUND).entity("Object not found.").build();  
-        }
-        return Response.ok(facture, MediaType.APPLICATION_JSON).build();   
+        } 
+        return Response.ok(invoiceList, MediaType.APPLICATION_JSON).build();
     }
-    
-    
-    @Path("/pms/facture-detail")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addTpmsFactureDetail(TPmsFactureDetail facture) {
-        try {
-            invoiceDao.setTpmsFactureDetail(facture);
-            return Response.status(Response.Status.CREATED).entity(facture).build();
-        } catch (CustomConstraintViolationException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
-    }
-    
-    @GET
-    @Path("/pos/facture")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTposFacture() {
-         List<TPosFacture> facture = invoiceDao.getTposFacture();        
-        if (facture == null) {
-            throw new NotFoundException();
-            //return Response.status(Response.Status.NOT_FOUND).entity("Object not found.").build();  
-        }
-        return Response.ok(facture, MediaType.APPLICATION_JSON).build();   
-    }
-    
-    
-    @Path("/pos/facture")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addTposFacture(TPosFacture facturePos) {
-        try {
-            invoiceDao.setTposFacture(facturePos);
-            return Response.status(Response.Status.CREATED).entity(facturePos).build();
-        } catch (CustomConstraintViolationException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
-    }
-    
-    @GET
-    @Path("/pos/facture-detail")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTposFactureDetail() {
-         List<TPosFactureDetail> facture = invoiceDao.getTposFactureDetail();        
-        if (facture == null) {
-            throw new NotFoundException();
-            //return Response.status(Response.Status.NOT_FOUND).entity("Object not found.").build();  
-        }
-        return Response.ok(facture, MediaType.APPLICATION_JSON).build();   
-    }
-    
-    
-    @Path("/pos/facture-detail")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addTposFactureDetail(TPosFactureDetail facture) {
-        try {
-            invoiceDao.setTposFactureDetail(facture);
-            return Response.status(Response.Status.CREATED).entity(facture).build();
-        } catch (CustomConstraintViolationException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
-    }
-    
-   
 }
